971286660799bdf2030a78926060a54b66a3010d
IndusYash <yashvardhanojha8217@gmail.com>
2025-12-09 19:55:12 +0530

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
+import ScrollToTop from './components/Common/ScrollToTop';
 import Home from './pages/Home';
 import Team from './pages/Team';
 import Events from './pages/Events';
@@ -13,6 +14,7 @@ import Canvas from './pages/Canvas';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
